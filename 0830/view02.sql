7bf462cdee38fe70f7ef3a39facf33a53cfcb9fb
Jimin Park <meanof5k@naver.com>
2023-08-31 11:55:09 +0900

    230830 dbms update

--- 0830/view02.sql
+++ 0830/view02.sql
@@ -4,6 +4,9 @@
 
 use market_db;
 
+-- 1. 뷰 생성
+-- : create view  구문 사용
+
 create view v_viewtest1
 as
 	select b.mem_id 'Member ID', m.mem_name as 'Member Name',
@@ -16,7 +19,7 @@ as
 -- 뷰를 조회하는 경우 열 이름에 공백이 있을 때 백틱으로 묶어줘야함
 select distinct `Member ID`, `Member Name` from v_viewtest1;
 
--- 뷰 수정
+-- 2. 뷰 수정
 -- : alter view 구문 사용
 
 alter view v_viewtest1
@@ -29,3 +32,13 @@ as
         on b.mem_id = m.mem_id;
 
 select distinct `회원 아이디`, `회원 이름` from v_viewtest1;
+
+-- 3. 뷰 삭제
+-- : drop view 구문 사용
+
+drop view v_viewtest1;
+
+-- 데이터베이스 개체는 서로 완전히 다른 기능을 하지만
+-- 생성/수정/삭제하는 문법은 거의 동일
+-- >>> 데이터베이스 개체: 데이터베이스 내에서 만들어지는 영역
+-- 				   ex) 테이블, 뷰, 스토어드 프로시저, 트리거 등
